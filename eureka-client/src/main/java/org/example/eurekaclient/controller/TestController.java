6c87828b36970a1b978bab2120e8ba60b7a7e693
Kseniya_Oznobishina <Kseniya_Oznobishina@epam.com>
2023-06-18 14:37:50 +0300

    5 commit - add config for running few instances

--- eureka-client/src/main/java/org/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/org/example/eurekaclient/controller/TestController.java
@@ -1,5 +1,6 @@
 package org.example.eurekaclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/main")
 public class TestController {
 
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
     @GetMapping("/test")
     public String test() {
-        return "test";
+        return id;
     }
 
 }
